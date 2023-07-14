542e063680d951e9bbb2119e8d9bd56b33568a6c
Sukanta Das <arnobh84@gmail.com>
2023-07-14 22:21:51 +0600

    extra code removed from add book page

--- src/components/Book/AddBook.tsx
+++ src/components/Book/AddBook.tsx
@@ -100,7 +100,7 @@ const AddBooks = () => {
                     </svg>
                   </button>
                   <button
-                    htmlFor="show_more"
+                    // htmlFor="show_more"
                     className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
                   >
                     <svg
@@ -155,7 +155,7 @@ const AddBooks = () => {
                     </svg>
                   </button>
                   <button
-                    htmlFor="show_more"
+                    // htmlFor="show_more"
                     className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
                   >
                     <svg
@@ -200,55 +200,6 @@ const AddBooks = () => {
                     My billing address is different than
                     above.
                   </label>
-                </div>
-              </div>
-
-              <div className="md:col-span-2">
-                <label htmlFor="soda">
-                  How many soda pops?
-                </label>
-                <div className="h-10 w-28 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                  <button
-                    htmlFor="show_more"
-                    className="cursor-pointer outline-none focus:outline-none border-r border-gray-200 transition-all text-gray-500 hover:text-blue-600"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4 mx-2"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                  <input
-                    name="soda"
-                    id="soda"
-                    placeholder="0"
-                    className="px-2 text-center appearance-none outline-none text-gray-800 w-full bg-transparent"
-                    value="0"
-                  />
-                  <button
-                    htmlFor="show_more"
-                    className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-500 hover:text-blue-600"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4 mx-2 fill-current"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                  </button>
                 </div>
               </div>
 
