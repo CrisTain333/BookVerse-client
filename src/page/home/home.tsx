d1102d99a265b2f5fc5a742bac34787e06a37907
Sukanta Das <arnobh84@gmail.com>
2023-07-14 19:53:40 +0600

    footer added

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,7 +1,57 @@
+import bookImage from "../../assets/book.jpg";
+
 const Home = () => {
   return (
     <div>
-      <p>Home Page</p>
+      <section className="py-24 w-[95%] mx-auto">
+        <div className="mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
+          <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
+            <h2 className="text-4xl text-gray-800 font-semibold md:text-5xl">
+              Welcome to BookVerse <br />
+              Discover a World of Books and Knowledge
+            </h2>
+            <p>
+              Dive into the vast collection of books and
+              explore new possibilities. BookVerse helps you
+              explore, learn, and connect with the world of
+              literature.
+            </p>
+            <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+              <a
+                href="javascript:void(0)"
+                className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none"
+              >
+                Let's get started
+              </a>
+              <a
+                href="javascript:void(0)"
+                className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex"
+              >
+                Get access
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
+            <img
+              src={bookImage}
+              className="shadow-lg md:rounded-tl-[108px]"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
