bfc418bbe1c69c8a3dab980646b49173ce6ac612
Sukanta Das <arnobh84@gmail.com>
2023-07-15 09:45:24 +0600

    add book page extra feildes removed

--- src/components/Book/AddBook.tsx
+++ src/components/Book/AddBook.tsx
@@ -1,4 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { useState } from "react";
+import { bookGenre } from "../../constant/genre";
+
 const AddBooks = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    genre: "Fiction",
+    publicationYear: "",
+  });
+
+  console.log(formData);
+
+  const handleAddBook = () => {};
+
   return (
     <div>
       <div className="bg-white rounded shadow-lg p-4 px-4 md:p-10 mb-6 w-[80%] mx-auto mt-10">
@@ -19,9 +35,16 @@ const AddBooks = () => {
                 <input
                   type="text"
                   name="title"
-                  id="full_name"
+                  onChange={(e) =>
+                    setFormData((prev: any) => {
+                      return {
+                        ...prev,
+                        title: e.target.value,
+                      };
+                    })
+                  }
                   className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
+                  value={formData?.title}
                 />
               </div>
 
@@ -30,183 +53,96 @@ const AddBooks = () => {
                 <input
                   type="text"
                   name="author"
-                  id="email"
                   className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
+                  onChange={(e) =>
+                    setFormData((prev: any) => {
+                      return {
+                        ...prev,
+                        author: e.target.value,
+                      };
+                    })
+                  }
                   placeholder=""
+                  value={formData?.author}
                 />
               </div>
 
               <div className="md:col-span-3">
-                <label htmlFor="address">
-                  Address / Street
+                <label htmlFor="year">
+                  Publication Year
                 </label>
-                <input
-                  type="text"
-                  name="address"
-                  id="address"
+
+                <div
+                  className="relative max-w-sm"
+                  id="year"
+                >
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                    <svg
+                      className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                    </svg>
+                  </div>
+                  <input
+                    id="year"
+                    type="date"
+                    value={formData?.publicationYear}
+                    onChange={(e) =>
+                      setFormData((prev: any) => {
+                        return {
+                          ...prev,
+                          publicationYear: e.target.value,
+                        };
+                      })
+                    }
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
+                    placeholder="Select date"
+                  />
+                </div>
+                {/* <input  name="" id="" /> */}
+              </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="city">Genre</label>
+                <br />
+                <select
+                  name="genre"
+                  value={formData?.genre}
+                  onChange={(e) =>
+                    setFormData((prev: any) => {
+                      return {
+                        ...prev,
+                        genre: e.target.value,
+                      };
+                    })
+                  }
                   className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
-                  placeholder=""
-                />
+                >
+                  {bookGenre?.map((e, i) => (
+                    <>
+                      {/* <option value="">Select Genre</option> */}
+                      <option
+                        value={e}
+                        key={i + 1}
+                      >
+                        {e}
+                      </option>
+                    </>
+                  ))}
+                </select>
               </div>
 
-              <div className="md:col-span-2">
-                <label htmlFor="city">City</label>
-                <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  value=""
-                  placeholder=""
-                />
-              </div>
-
-              <div className="md:col-span-2">
-                <label htmlFor="country">
-                  Country / region
-                </label>
-                <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                  <input
-                    name="country"
-                    id="country"
-                    placeholder="Country"
-                    className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-                    value=""
-                  />
-                  <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
-                    <svg
-                      className="w-4 h-4 mx-2 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <line
-                        x1="18"
-                        y1="6"
-                        x2="6"
-                        y2="18"
-                      ></line>
-                      <line
-                        x1="6"
-                        y1="6"
-                        x2="18"
-                        y2="18"
-                      ></line>
-                    </svg>
-                  </button>
-                  <button
-                    // htmlFor="show_more"
-                    className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
-                  >
-                    <svg
-                      className="w-4 h-4 mx-2 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <polyline points="18 15 12 9 6 15"></polyline>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              <div className="md:col-span-2">
-                <label htmlFor="state">
-                  State / province
-                </label>
-                <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                  <input
-                    name="state"
-                    id="state"
-                    placeholder="State"
-                    className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-                    value=""
-                  />
-                  <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
-                    <svg
-                      className="w-4 h-4 mx-2 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <line
-                        x1="18"
-                        y1="6"
-                        x2="6"
-                        y2="18"
-                      ></line>
-                      <line
-                        x1="6"
-                        y1="6"
-                        x2="18"
-                        y2="18"
-                      ></line>
-                    </svg>
-                  </button>
-                  <button
-                    // htmlFor="show_more"
-                    className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
-                  >
-                    <svg
-                      className="w-4 h-4 mx-2 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <polyline points="18 15 12 9 6 15"></polyline>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              <div className="md:col-span-1">
-                <label htmlFor="zipcode">Zipcode</label>
-                <input
-                  type="text"
-                  name="zipcode"
-                  id="zipcode"
-                  className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                  placeholder=""
-                  value=""
-                />
-              </div>
-
-              <div className="md:col-span-5">
-                <div className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    name="billing_same"
-                    id="billing_same"
-                    className="form-checkbox"
-                  />
-                  <label
-                    htmlFor="billing_same"
-                    className="ml-2"
-                  >
-                    My billing address is different than
-                    above.
-                  </label>
-                </div>
-              </div>
-
-              <div className="md:col-span-5 text-right">
+              <div
+                className="md:col-span-5 text-right"
+                onClick={handleAddBook}
+              >
                 <div className="inline-flex items-end">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Submit
+                    Add
                   </button>
                 </div>
               </div>
